06dc219f0f0efe2294b0c1beecfe4c4205dc752b
MMM <1234@163.com>
2024-04-12 18:21:09 +0800

    init ffmpeg

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
 #include <string>
+#include "getopt.h"
+#include "gperf.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myyffmpeg_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+//    uint64_t i = GetTicks();
     return env->NewStringUTF(hello.c_str());
 }
