afbb765139e7d2c3d576610d77b48020146b2259
MMM <1234@163.com>
2024-04-13 15:03:20 +0800

    cmakelists init

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ Java_com_example_myyffmpeg_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-//    uint64_t i = GetTicks();
+    uint64_t i = GetTicks();
     return env->NewStringUTF(hello.c_str());
 }
